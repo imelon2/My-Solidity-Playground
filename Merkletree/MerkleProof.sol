d6167ceee0e4a293eeb3b5100d936ecba9d2bc49
imelon2 <imelon279@naver.com>
2023-01-29 12:56:48 +0900

    MerkleProof

--- Merkletree/MerkleProof.sol
+++ Merkletree/MerkleProof.sol
@@ -4,25 +4,20 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract Merkle {
-    // mapping (bytes32 => mapping (bytes32 => bytes32)) public m_merkleRoot;
-    mapping (bytes32 => bytes32) public _merkleRoot;
+    bytes32 public _merkleRoot;
 
-    function setMerkleRoot(bytes32 title, bytes32 root) external {
-        _merkleRoot[title] = root;
+    function setMerkleRoot(bytes32 root) external {
+        _merkleRoot = root;
     }
 
-    function getString(string memory title) public pure returns(bytes32) {
-        return keccak256(abi.encodePacked(title));
-    }
-
-    function canClaim(bytes32 title, address claimer, bytes32[] calldata merkleProof)
+    function canClaim(address claimer, bytes32[] calldata merkleProof)
         public
         view
         returns (bool)
     {
         return MerkleProof.verify(
                 merkleProof,
-                _merkleRoot[title],
+                _merkleRoot,
                 keccak256(abi.encodePacked(claimer))
             );
     }
